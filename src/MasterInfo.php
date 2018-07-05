67854502ce02dfeb7cb067eac661f559b63d0c24
TechartWebDev <webdev@techart.ru>
2018-07-05 09:46:21 +0300

    Version 1.0.10

---
+++ src/MasterInfo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Techart\API;
+
+class MasterInfo
+{
+    public static function getData($code)
+    {
+        return Cache::get("master-info-{$code}.json", function() use ($code) {
+            return API::getStruct("/master-info/{$code}/");
+        });
+    }
+    
+    public static function getText($code)
+    {
+        $data = self::getData($code);
+        if ($data) {
+            return $data->content;
+        }
+    }
+}
