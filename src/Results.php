da0b06751350a1b72b087a93a4587110ff5ffde2
Git Techart <git@tchart.ru>
2024-01-29 05:40:54 +0000

    Version 1.6.0

--- src/Results.php
+++ src/Results.php
@@ -7,6 +7,15 @@ class Results
 	public static function get($id)
 	{
 		return Cache::get("results-{$id}.json", function() use($id) {
-			return API::getStruct("results/{$id}.json");});
+			return API::getStruct("results/{$id}.json");
+		});
+	}
+
+	public static function getPortfolio($recipient, $date)
+	{
+		if ($date) {
+			return API::getStruct("/portfolio/{$recipient}/{$date}/");
+		}
+		return API::getStruct("/portfolio/{$recipient}/");
 	}
 }
