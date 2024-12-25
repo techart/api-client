f794bfa24b49ef15aaf52d5fe6065a76fc7877c2
Git Techart <git@tchart.ru>
2024-12-25 12:35:41 +0000

    Version 1.7.0

--- src/MasterInfo.php
+++ src/MasterInfo.php
@@ -6,6 +6,11 @@ class MasterInfo
 {
 	public static function getData($code)
 	{
+		if ($code == 'vita-divisions') {
+			return Cache::get("master-info-{$code}.json", function() use ($code) {
+				return API::getStruct("/divisions/");
+			});
+		}
 		return Cache::get("master-info-{$code}.json", function() use ($code) {
 			return API::getStruct("/master-info/{$code}/");
 		});
